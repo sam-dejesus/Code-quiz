3e1b44d12b1823f6fbb84b5d8375c204dbacb538
samuel de jesus <samue@MacBook-Air.local>
2023-03-16 17:38:25 -0400

    added functions that create buttons and questions based on the questions object as well as create a clock that works as soon as start button is pressed. made it possible when the correct answer is picked the dom will update to the next question

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,19 @@
-var countdowntime = 600;
+
+var startButton = document.getElementById('start-btn')
+var questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+let currentQuestionIndex = undefined;
 var correct = 0;
 var incorrect = 0;
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+   startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
+//------------------------------
+var countdowntime = 600;
+
 var countdownEL = document.getElementById("time");
 var countdown = setInterval(function(){
    var minutes = Math.floor(countdowntime / 60);
@@ -10,17 +23,83 @@ var countdown = setInterval(function(){
    if (countdowntime < 0){
       clearInterval(countdown);
       countdownEL.textContent = "times up"
+      gameOver();
    }
 }, 1000);
-var submitBtn = document.getElementById('submit');
-submitBtn.addEventListener('click', Response);
-
-function Response(){
-var answersone = "b";
-var yesone = document.getElementById("yes").checked
-
-if (yesone === true){
-    alert("hiiii")
+//--------------------
+currentQuestionIndex = 0;
+showQuestion()
 }
 
+function showQuestion(){
+    const question = questions[currentQuestionIndex];
+   questionElement.innerText = question.question;
+   answerButtonsElement.innerHTML = '';
+   question.answers.forEach(answer => {
+       const button = document.createElement('button');
+       button.innerText = answer.text;
+       button.classList.add('btn');
+       if (answer.correct){
+           button.dataset.correct = answer.correct;
+       }
+       button.addEventListener('click',selectAnswer);
+       answerButtonsElement.appendChild(button);
+   })
    }
+//-----------
+function selectAnswer(e){
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    if (correct){
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions.length){
+            showQuestion();
+        }else{
+            gameOver();
+        }
+    } else {
+        incorrect++;
+    }
+}
+function gameOver (){
+
+}
+   const questions = [
+      {
+       question: 'what is 2+2',
+       answers:[
+           {text: '4', correct:true},
+           {text:'22', correct:false}
+       ]
+      },
+      {
+       question: 'what is 2+0',
+       answers:[
+           {text: '4', correct:true},
+           {text:'22', correct:false}
+       ]
+      },
+      {
+       question: 'what is 2+222',
+       answers:[
+           {text: '4', correct:true},
+           {text:'22', correct:false}
+       ]
+      },
+      {
+       question: 'what is 2+22',
+       answers:[
+           {text: '4', correct:true},
+           {text:'22', correct:false}
+       ]
+      },
+      {
+       question: 'what is 2+1',
+       answers:[
+           {text: '4', correct:true},
+           {text:'22', correct:false}
+       ]
+      }
+   ]
+
+
